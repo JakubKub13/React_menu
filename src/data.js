955b5068184334f25020fa50257666d03af81473
JakubKub13 <jakubkubala3@gmail.com>
2023-07-06 08:25:03 +0200

    finishing menu

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const links = [
+    {
+        id: 1,
+        url: "/",
+        text: "Home",
+    },
+    {
+        id: 2,
+        url: "/meetings",
+        text: "Meetings",
+    },
+    {
+        id: 3,
+        url: "/movies",
+        text: "Movies",
+    },
+    {
+        id: 4,
+        url: "/mylist",
+        text: "My list",
+    },
+]
+
+
+export default links
